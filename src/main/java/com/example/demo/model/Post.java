e6072e54b6767096068ddfa3c32e29bb02a53be2
Balandala <balandala05@gmail.com>
2025-02-24 02:30:54 +0500

    added Bootstrap CSS

--- src/main/java/com/example/demo/model/Post.java
+++ src/main/java/com/example/demo/model/Post.java
@@ -1,17 +1,25 @@
 package com.example.demo.model;
 
+import java.util.Random;
+
 public class Post {
     private final String text;
+    private final String creationDate;
     private Integer likes;
 
-    public Post(String text, int likes){
+    public Post(String text, String creationDate){
         this.text =text;
-        this.likes = likes;
+        this.creationDate = creationDate;
+        Random rnd = new Random();
+        this.likes = rnd.nextInt(1000);
     }
     public int getLikes(){
         return likes;
     }
     public String getText(){
         return text;
+    }
+    public String getCreationDate(){
+        return creationDate;
     }
 }
--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -9,9 +9,9 @@ import java.util.ArrayList;
 public class PostService {
     public ArrayList<Post> listAllPosts(){
         ArrayList<Post> posts = new ArrayList<>();
-        posts.add(new Post("Котики - это круто!", 964));
-        posts.add(new Post("Что бы спина не болела надо всего лишь перед сном...", 10));
-        posts.add(new Post("Абоба", -1));
+        posts.add(new Post("Котики - это круто!", "29.07.2024"));
+        posts.add(new Post("Что бы спина не болела надо всего лишь перед сном...", "02.02.2002"));
+        posts.add(new Post("Абоба", "24.02.2025"));
         return  posts;
     }
 }
